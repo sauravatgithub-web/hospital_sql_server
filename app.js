3c32fcce23b11db9a71c0d85274f76244030273d
sauravatgithub-web <22cs01010@iitbbs.ac.in>
2025-04-04 00:52:27 +0530

    Added basic files

---
+++ app.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import helmet from 'helmet';
+import http from 'http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import { connectDB } from './utils/features.js';
+import { errorMiddleware } from './middlewares/error.js';
+
+
+
+const corsOptions = {
+    origin: [],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}
+
+dotenv.config({ path: './config.env' })
+
+const mongoURI = process.env.MONGO_URI;
+const port = process.env.PORT || 4173;
+
+connectDB(mongoURI);
+
+const app = express();
+const server = http.createServer(app);
+
+app.disable('x-powered-by');
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", 'trusted-cdn.com'],
+        }
+    },
+    frameguard: { action: 'deny' },
+}));  
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to MySQL project');
+})
+
+app.use(errorMiddleware);
+
+server.listen(port, () => {
+    console.log(`Server is currently active on port ${port}`);
+})
---
+++ utils/features.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+const cookieOption = {
+    maxAge: 15 * 24 * 60 * 60 * 1000, 
+    sameSite: "none", 
+    httpOnly: true, 
+    secure: true, 
+};
+
+const connectDB = (uri) => {
+    mongoose
+        .connect(uri, { dbname: "Hospital" })
+        .then((data) => console.log(`Connected to MongoDB database: ${data}`))
+        .catch((err) => { throw err })
+};
+
+const sendToken = (res, user, code, message) => {
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    return res.status(code).cookie('h_token', token, cookieOption).json({
+        success: true,
+        user,
+        message,
+        role: user.role
+    })
+};
+
+export { cookieOption, connectDB, sendToken };
