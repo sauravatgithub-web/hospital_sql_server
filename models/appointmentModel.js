d7b341212dddcfaf362865c841b36addf270ffaf
sauravatgithub-web <22cs01010@iitbbs.ac.in>
2025-04-08 15:49:13 +0530

    Updated appointment model

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -16,13 +16,22 @@ const appointmentSchema = new mongoose.Schema({
   patient: { type: Types.ObjectId, ref: "Patient" },
   doctor: { type: Types.ObjectId, ref: "Doctor" },
   nurse: [{ type: Types.ObjectId, ref: "Nurse" }],
-  prescription: [{ type: Types.ObjectId, ref: "Drug" }],
+  remarks: [{ 
+    remarkTime: { type: Date, default: Date.now },
+    remarkMsg: { type: String }
+  }],
   tests: [{ type: Types.ObjectId, ref: "Test" }],
   hps: [{ type: Types.ObjectId, ref: "Hospital_Professional" }],
   hs: [{ type: Types.ObjectId, ref: "Hospital_Staff" }],
   disease: [{ type: Types.ObjectId, ref: "Disease" }],
   assignedRoom: { type: Types.ObjectId, ref: "Room" },
-  drugs: [{ type: Types.ObjectId, ref: "Drug" }],
+  drugs: [{
+    drug: { 
+      type: Types.ObjectId, 
+      ref: "Drug" 
+    },
+    dosage: { type: String }
+  }],
 });
 
 
